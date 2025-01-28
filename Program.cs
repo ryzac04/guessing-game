f134c2094482314d067685789fcd9e65fbde07c0
ryzac04 <rzc044@gmail.com>
2025-01-28 12:03:47 -0600

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Namespace
+namespace NumberGuesser
+{
+    // Main Class
+    class Program
+    {
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    
+}
